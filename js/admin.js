e3762e2e1c841e34f3281393a9124d9743ab20f7
Mateo Lohezic <mateolohezic@gmail.com>
2022-10-21 18:00:01 -0300

    Añadir juego

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,31 @@
+const getJuegos = async () => {
+    const resultado = await fetch("http://localhost:3000/games");
+    const juegos = await resultado;
+    return console.log(juegos);
+}
+
+getJuegos()
+
+// const imprimirTabla = async () => {
+//     const juegoss = await cargarJuegos();
+//     const fila = document.getElementById ("tabla");
+
+//     const filas = juegoss.map(juego => (`
+//     <tr>
+//         <td>${juego.id}</td>
+//         <td>${juego.title}</td>
+//         <td>${juego.categorie}</td>
+//         <td>${juego.synopsis}</td>
+//         <td>$ ${juego.price}</td>
+//         <td>Publicado</td>
+//         <td>Opciones</td>
+//     </tr>
+//     `))
+//     fila.innerHTML = filas
+// }
+
+// imprimirTabla()
+
 const crearJuego = () =>{
         const title = document.getElementById ("title").value
         const date = document.getElementById ("date").value
@@ -6,7 +34,6 @@ const crearJuego = () =>{
         const categorie = document.getElementById ("categorie").value
         const developer = document.getElementById ("developer").value
         const rating = document.getElementById ("rating").value
-        const favorite = document.getElementById ("favorite").value
         const image1 = document.getElementById ("image1").value
         const image2 = document.getElementById ("image2").value
         const image3 = document.getElementById ("image3").value
@@ -22,7 +49,6 @@ const crearJuego = () =>{
                 categorie,
                 developer,
                 rating,
-                favorite,
                 image1,
                 image2,
                 image3,
